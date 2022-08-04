5354f3e149924030d8fee86ac9dcf2da50bc34ca
Moses-Obaro <36534768+Moses-Obaro@users.noreply.github.com>
2022-08-04 15:41:11 +0100

    Add files via upload

---
+++ Owoda/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Owoda owomida = new Owoda();
+            owomida.Start();
+        }
+    }
+}
+
