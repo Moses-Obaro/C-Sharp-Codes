937ccaead654a9a8234c83a44a50f6573f10b710
Moses Obaro <36534768+Moses-Obaro@users.noreply.github.com>
2022-08-10 07:06:49 +0100

    Add files via upload

---
+++ InecApplication/InecApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace InecApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            VoterDataBase newDB = new VoterDataBase();
+            //newDB.DisplayExistingDB();
+           
+            newDB.GetVotersCount(newDB.existingDataBase);
+            Console.WriteLine();
+            Console.WriteLine();
+            newDB.DisplayCitizenData(newDB.RemoveDuplicateEntries(newDB.existingDataBase));
+            
+
+            
+        }
+    }
+}
